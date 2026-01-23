74b6625c1a475730b337379f9e92b416280ac81f
Calvin D. <cdevogel25@gmail.com>
2026-01-23 16:36:37 -0600

    contact under construction

--- src/app/components/pages/contact/contact.ts
+++ src/app/components/pages/contact/contact.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageLayout } from '../../page-layout/page-layout';
+import { FeatherModule } from "angular-feather";
+import { IconsModule } from '../../../modules/icons/icons-module';
 
 @Component({
   selector: 'app-contact',
-  imports: [PageLayout, CommonModule],
+  imports: [PageLayout, CommonModule, IconsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
--- src/app/modules/icons/icons-module.ts
+++ src/app/modules/icons/icons-module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatherModule } from 'angular-feather';
-import { Github, Linkedin, Home, FileText, Codepen } from 'angular-feather/icons';
+import { Github, Linkedin, Home, FileText, Codepen, AlertTriangle } from 'angular-feather/icons';
 
 export const icons = {
   Github,
   Linkedin,
   Home,
   FileText,
-  Codepen
+  Codepen,
+  AlertTriangle
 }
 
 
