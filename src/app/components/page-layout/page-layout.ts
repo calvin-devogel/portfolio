7c835c0483263df3b74ece9930765f6819d4346e
Calvin D. <cdevogel25@gmail.com>
2026-01-23 12:29:59 -0600

    dont just blast your email out there, dummy

--- src/app/components/page-layout/page-layout.ts
+++ src/app/components/page-layout/page-layout.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-page-layout',
-  imports: [],
+  imports: [Footer],
   templateUrl: './page-layout.html',
   styleUrl: './page-layout.scss',
-  standalone: true,
 })
 export class PageLayout {
 
