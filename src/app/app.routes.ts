aba309fd7d2489d1aa56cdaebc66b80db33d22f4
Calvin D. <cdevogel25@gmail.com>
2026-01-23 12:45:53 -0600

    dummy contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
 import { Resume } from './components/pages/resume/resume';
 import { Projects } from './components/pages/projects/projects';
+import { Contact } from './components/pages/contact/contact';
 
 export const routes: Routes = [
     { path: '', component: Home, pathMatch: 'full' },
     { path: 'resume', component: Resume },
     { path: 'projects', component: Projects },
     { path: 'health_check', redirectTo: '' },
+    { path: 'contact', component: Contact },
     { path: '**', redirectTo: '', pathMatch: 'full'}, 
 ];
---
+++ src/app/components/pages/contact/contact.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageLayout } from '../../page-layout/page-layout';
+
+@Component({
+  selector: 'app-contact',
+  imports: [PageLayout, CommonModule],
+  templateUrl: './contact.html',
+  styleUrl: './contact.scss',
+})
+export class Contact {
+
+}
