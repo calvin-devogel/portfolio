d6e6f9e393f6455466656365eb3976bde1c7ad46
Calvin D. <cdevogel25@gmail.com>
2026-02-05 06:29:35 -0600

    re-included angular routing section, added sudo to mv in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ envsubst '${API_URL}' < /usr/share/nginx/html/assets/env.template.ts > /usr/shar
 
 EXPORT BACKEND_URL=$API_URL
 envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
-mv /tmp/nginx.conf /etc/nginx/nginx.conf
+sudo mv /tmp/nginx.conf /etc/nginx/nginx.conf
 
 exec "$@"
