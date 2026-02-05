af02edf090e572a77ac865b49dbe770152bbc81f
Calvin D. <cdevogel25@gmail.com>
2026-02-05 09:35:01 -0600

    forgot a semicolon

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,4 @@
 
 envsubst '${API_URL}' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
 
-EXPORT BACKEND_URL=$API_URL
-envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
-sudo mv /tmp/nginx.conf /etc/nginx/nginx.conf
-
 exec "$@"
