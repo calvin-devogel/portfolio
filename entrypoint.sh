2c753d5fdc069aeaaa161a9e80a87329e950be7a
Calvin D. <cdevogel25@gmail.com>
2026-02-05 10:13:10 -0600

    another attempt at using environment variables for api config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# substitue API_URL in env.js
 envsubst '${API_URL}' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
+
+# substitue API_URL in nginx.conf
+envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
