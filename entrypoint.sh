8c6b5575c94dccfe48e44738721c5e94623c88aa
Calvin D. <cdevogel25@gmail.com>
2026-02-04 22:12:35 -0600

    more spaghetti

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 envsubst '${API_URL}' < /usr/share/nginx/html/assets/env.template.ts > /usr/share/nginx/html/assets/env.ts
 
-EXPORT BACKEND_URL=${API_URL}
-envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conv > /tmp/nginx.conf
+EXPORT BACKEND_URL=$API_URL
+envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
 mv /tmp/nginx.conf /etc/nginx/nginx.conf
 
 exec "$@"
