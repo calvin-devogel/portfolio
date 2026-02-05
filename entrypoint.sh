f1be9f87cc3606e233ad38a3e31d23eaa411ecd2
Calvin D. <cdevogel25@gmail.com>
2026-02-05 09:26:09 -0600

    try proxying with the bare server url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-envsubst '${API_URL}' < /usr/share/nginx/html/assets/env.template.ts > /usr/share/nginx/html/assets/env.ts
+envsubst '${API_URL}' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
 
 EXPORT BACKEND_URL=$API_URL
 envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
