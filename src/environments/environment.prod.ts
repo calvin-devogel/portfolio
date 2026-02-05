7f630b54fbbb575c612133bc0bde6c31c50fc1c6
Calvin D. <cdevogel25@gmail.com>
2026-02-04 21:07:24 -0600

    attempted fix at api env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
+import { isPlatformBrowser } from "@angular/common";
+import { inject, PLATFORM_ID } from "@angular/core";
+
 export const environment = {
     production: true,
-    // how can I pass this as an env variable??
-    apiUrl: (window as any).__env?.apiUrl || ''
+    get apiUrl() {
+        const platformId = inject(PLATFORM_ID);
+        if (isPlatformBrowser(platformId)) {
+            return (window as any).__env?.apiUrl || '';
+        }
+        return '';
+    }
 };
