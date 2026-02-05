18ee639b34f9754686acc544f23aef7354ac3a1a
Calvin D. <cdevogel25@gmail.com>
2026-02-05 10:25:10 -0600

    swapping stuff around, hoping it works

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@
 envsubst '${API_URL}' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
 
 # substitue API_URL in nginx.conf
-envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${API_URL}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.template
 
 exec "$@"
