acb89f3ab762a3aa8d877d78011d3fb2ae15adde
Calvin D. <cdevogel25@gmail.com>
2026-02-11 12:17:34 -0600

    edited idempotency generator to try to use crypto first

--- src/app/services/contact/message-service.ts
+++ src/app/services/contact/message-service.ts
@@ -55,11 +55,15 @@ export class MessageService {
 
   // generate uuid v4 for idempotency key
   private generateIdempotencyKey(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-      const r = Math.random() * 16 | 0;
-      const v = c === 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
+    if (crypto && crypto.randomUUID) {
+      return crypto.randomUUID();
+    } else {
+      // Fallback for environments without crypto.randomUUID
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    }
   }
   
 }
