a9a3478277251a4287123099b62217b67aecab7b
Calvin D. <cdevogel25@gmail.com>
2026-02-23 13:09:39 -0600

    continued visual upgrades, struggling with the navbar on mobile.

--- src/app/components/pages/projects/projects.ts
+++ src/app/components/pages/projects/projects.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PageLayout } from '../../page-layout/page-layout';
+import { FeatherModule } from 'angular-feather';
 
 @Component({
   selector: 'app-projects',
-  imports: [PageLayout],
+  imports: [PageLayout, FeatherModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
